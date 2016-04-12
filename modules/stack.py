fab9782631668024d508d44b4ff1e7e53eea296c
alozovskoy <alozovskoy@gmail.com>
2016-04-12 22:55:56 +0300

    Переделал список хранения данных о видео - теперь вместо отдельных списков используется словарь с ID видео в качестве ключей; длительность, имя видео и т.д. хранится внутри этого словаря

--- modules/stack.py
+++ modules/stack.py
@@ -6,14 +6,13 @@ import json
 
 class Stack(object):
     def __init__(self):
-        self.items = []
-        self.names = {}
-        self.duration = {}
+        self.items = {}
+        self.itemsList = []
         self.current = ''
         
     def getName(self, item):
-        if item in self.names.keys():
-            return self.names[item]
+        if item in self.items.keys():
+            return self.items[item]['name']
         else:
             return None
 
@@ -21,22 +20,23 @@ class Stack(object):
         return self.current
 
     def getTime(self, item):
-        if item in self.duration.keys():
-            return self.duration[item]
+        if item in self.items.keys():
+            return self.items[item]['duration']
         else:
             return None
 
     def isEmpty(self):
-        return self.items == []
+        return len(self.itemsList) == 0
 
     def push(self, item):
-        if not item in self.items:
+        if not item in self.items.keys():
             title = youtube.getTitle(item)
             duration = youtube.getDuration(item)
             if title and duration:
-                self.items.append(str(item))
-                self.names[item] = title
-                self.duration[item] = int(isodate.parse_duration(duration).total_seconds())
+                self.items[str(item)] = {}
+                self.itemsList.append(str(item))
+                self.items[str(item)]['name'] = title
+                self.items[str(item)]['duration'] = int(isodate.parse_duration(duration).total_seconds())
                 return True
             else:
                 return False
@@ -45,29 +45,32 @@ class Stack(object):
 
     def pop(self):
         if not self.isEmpty():
-            self.current = self.items.pop(0)
-            logging.info(self.current)
+            self.current = self.itemsList.pop(0)
         return None
 
     def last(self):
-        return self.items[len(self.items)-1]
+        if not self.isEmpty():
+            return self.itemsList[len(self.itemsList)-1]
+        else:
+            return None
 
     def first(self):
         if not self.isEmpty():
-            return self.items[0]
+            return self.itemsList[0]
         else:
             return None
 
     def size(self):
-        return len(self.items)
+        return len(self.itemsList)
 
     def delete(self, item):
-        self.items.remove(item)
+        self.itemsList.remove(item)
+        self.items.pop(item, None)
         return None
 
     def get(self):
         data = {}
-        for i in enumerate(self.items):
-            _data = { 'item' : i[1], 'name' : self.names[i[1]], 'duration' : self.duration[i[1]] }
+        for i in enumerate(self.items.keys()):
+            _data = { 'item' : i[1], 'name' : self.items[i[1]]['name'], 'duration' : self.items[i[1]]['duration'] }
             data[i[0]] = json.dumps(_data)
         return json.dumps(data)
