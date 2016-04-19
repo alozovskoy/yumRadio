9e6a830f594a4a57cf3c197cc019ae3d3f2503bf
alozovskoy <alozovskoy@gmail.com>
2016-04-19 16:28:42 +0300

    Удалил статику из файла для импорта

--- templates/js/profile.js
+++ templates/js/profile.js
@@ -4,20 +4,4 @@
 {% include static/js/alert.js %}
 {% include static/js/wsSendMessage.js %}
 {% include static/js/wsOnMessage.js %}
-
-function checkSessionData(){
-    document.getElementById("userid").innerHTML = getCookie('userid');
-    document.getElementById("sessionid").innerHTML = getCookie('sessionid');
-}
-
-function logoutThis(){
-    deleteCookie('userid');
-    deleteCookie('sessionid');
-    checkSessionData();
-}
-
-function logoutAll(){
-    sendMessage(JSON.stringify({type: "session", action: "dropsession"}));  
-}
-
-checkSessionData();
+{% include static/js/profile.js %}
