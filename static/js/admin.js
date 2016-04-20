dc49a9ae8df0714cd11bbe312b19e84694c90c93
alozovskoy <alozovskoy@gmail.com>
2016-04-20 15:06:20 +0300

    Разделил админку на странички, добавил навигацию между страницами

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,1 +1,31 @@
 $adminkey = $("input[name='adminkey']");
+
+var divs = [
+    'queueRow',
+    'usersRow',
+    'banRow',
+    'sendRow'
+];
+
+var activeButton = 'queueButton'
+
+function showPage(page, button, nobuttons = false){
+    if ( !nobuttons ){
+        $('#' + activeButton).removeClass('btn-success');
+        $('#' + activeButton).addClass('btn-primary');
+        
+        activeButton = button;
+    
+        $('#' + activeButton).removeClass('btn-primary');
+        $('#' + activeButton).addClass('btn-success');
+    }
+    for (var i = 0; i < divs.length; i++){
+        if ( page == divs[i] ){
+            $('#' + divs[i]).show();
+        } else {
+            $('#' + divs[i]).hide();
+        }
+    }
+}
+
+showPage('queueRow', 'queueButton', true);
