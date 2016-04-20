9a01989d7d26e79d6fac1858acd0d99ac6dd11d7
alozovskoy <alozovskoy@gmail.com>
2016-04-20 05:01:54 +0300

    Добавил отображение превью в очереди, нумерацию треков и обратную их сортировку

--- static/js/queue.js
+++ static/js/queue.js
@@ -5,15 +5,18 @@ function videoGetQueue(){
 
 function wsOnQueue(data){
 	var queue = JSON.parse(data);
-	var tableHead = '<table class="table table-striped table-condensed table-bordered"><tbody>'
+	var tableHead = '<table class="table table-striped table-condensed"><tbody>'
 	var tableTail = '</tbody></table>'
     var tableData = ''
 
 	if ( Object.keys(queue).length > 0 ){
-		for (var key = 0; key < Object.keys(queue).length ; key++) {
-			var item = JSON.parse(queue[key]);
-			time = parseInt(item['duration'])
-			tableData += '<tr><td>NaN</td><td><a class="btn btn-default btn-block" href="https://www.youtube.com/watch?v=' + item['item'] + '">' + item['name'] + ', ' + toFormattedTime(time, true, false)  + '</td></tr>'
+		for (var key = Object.keys(queue).length; key > 0 ; key--) {
+			var item = JSON.parse(queue[key - 1]);
+            var next = '';
+			time = parseInt(item['duration']);
+			tableData += '<tr><td>' + key + '</td><td class="text-center">\
+                <a href="https://www.youtube.com/watch?v='+ item['item'] + '" style="display: block;">\
+                    <img src="' + item['thumbnail'] + '" style="height: 32px;"/></a></td><td><a class="btn btn-block" href="https://www.youtube.com/watch?v='+ item['item'] + '" style="display: block;">' + item['name'] + ', ' + toFormattedTime(time, true, false)+ '</a></td></td></tr>'
 		}
 	}
 	else {
