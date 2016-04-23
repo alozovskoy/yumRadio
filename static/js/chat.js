d39cd7473c232069aba29f1ee5d318f0c5b4bc27
alozovskoy <alozovskoy@gmail.com>
2016-04-23 21:32:36 +0300

    Убрал эскейпинг у системных сообщений

--- static/js/chat.js
+++ static/js/chat.js
@@ -19,6 +19,8 @@ function intToRGB(i){
 function chatGetMessage(name, sender, msg){
     var username = '';
     var message = '';
+    var usernamecolor = '';
+    var text = '';
     
     if (message.startsWith('http') && (message.endsWith('.png') || message.endsWith('.jpg') || message.endsWith('.gif'))){
         message = '<a href="' + message + '"><img src="' + message + '" height="50px"></a>'
@@ -28,16 +30,20 @@ function chatGetMessage(name, sender, msg){
         username = name;
         message = msg
         
-        var text = '<span style="color: #fea610;"> &#10047; ' + username + '</span>: ' + message;
+        if (username == 'system'){
+            username = '✿ ' + username;
+            usernamecolor = 'fea610;';
+        } else {
+            usernamecolor = intToRGB(hashCode(username));
+        }
     } else {
         username = safe_tags_replace(name);
-        message = safe_tags_replace(msg)
-        var usernamecolor = intToRGB(hashCode(username));
-        var text = '<span style="color: #' + usernamecolor + ';">' + username + '</span>: ' + message;
+        message = safe_tags_replace(msg);
+        usernamecolor = intToRGB(hashCode(username));
     }
 
+    text = '<span style="color: #' + usernamecolor + ';">' + username + '</span>: ' + message;
 	$('#chatframe').append('<p>' + text + '</p>');
-
 	document.getElementById("chatframe").scrollTop = document.getElementById("chatframe").scrollHeight;
 }
 
