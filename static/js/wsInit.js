806b02a6fe34413b7427eb5d236193bbe9230c14
alozovskoy <alozovskoy@gmail.com>
2016-04-30 03:59:08 +0300

    Перенес функцию отправки сообщений из отдельного файла поближе к инициализации WS

--- static/js/wsInit.js
+++ static/js/wsInit.js
@@ -26,3 +26,13 @@ function waitForSocketConnection(socket, callback){
             }
         }, 100);
 }
+
+function sendMessage(msg){
+    waitForSocketConnection(ws, function(){
+        var _msg = JSON.parse(msg);
+        _msg['userid'] = getCookie('userid');
+        _msg['sessionid'] = getCookie('sessionid');
+        dataToSend = JSON.stringify(_msg);
+        ws.send(dataToSend);
+    });
+};
--- static/js/wsSendMessage.js
+++
@@ -1,9 +0,0 @@
-function sendMessage(msg){
-    waitForSocketConnection(ws, function(){
-        var _msg = JSON.parse(msg);
-        _msg['userid'] = getCookie('userid');
-        _msg['sessionid'] = getCookie('sessionid');
-        dataToSend = JSON.stringify(_msg);
-        ws.send(dataToSend);
-    });
-}
